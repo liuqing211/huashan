5cf1b25df19040348b92ec22912dc63b080f4865
Lun Liu <86.81.131.46@86.81.131.46.com>
2021-01-04 11:37:13 +0800

    清理脏数据增加白名单

--- src/main/java/com/kedacom/haiou/kmtool/controller/CleanDirtyDataController.java
+++ src/main/java/com/kedacom/haiou/kmtool/controller/CleanDirtyDataController.java
@@ -7,12 +7,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.CollectionUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
@@ -27,23 +25,28 @@ public class CleanDirtyDataController {
     @Autowired
     private CleanDirtyDataService cleanDirtyDataService;
 
+    @Value("${whitelist.repoId}")
+    private String whiteRepeIds;
+
     @PostMapping("/staticface/cleanByTabCreatorId")
     @ResponseBody
-    public String uploadLocalPic(@RequestParam String creatorId) {
+    public String cleanByTabCreatorId(@RequestBody Map<String, Object> param) {
+        final String creatorId = param.get("creatorId").toString();
         log.info("收到根据库创建人 {} 清理 staticface 中脏数据的请求", creatorId);
         List<HaiouRepository> haiouRepositoryList = cleanDirtyDataService.getTabByCreatorId(creatorId);
         if (CollectionUtils.isEmpty(haiouRepositoryList)) {
             return "This user doesn't created repository";
         }
 
+        List<String> whiteRepoIdList = Arrays.asList(whiteRepeIds.split(","));
         log.info("根据库创建人 {} 查询到的人员库有 {}", creatorId, haiouRepositoryList.size());
         List<HaiouRepository> cleanRepositoryList = new ArrayList<>();
         haiouRepositoryList.forEach(haiouRepository -> {
             final String tabId = haiouRepository.getId();
             if (StringUtils.isNotBlank(tabId)) {
                 boolean isNotEmpty = cleanDirtyDataService.confirmStaticfaceNum(tabId);
-                if (!isNotEmpty) {
-                    log.info("人员库 {}-{} 中无数据，无需清理", haiouRepository.getId(), haiouRepository.getName());
+                if (!isNotEmpty || whiteRepoIdList.contains(tabId)) {
+                    log.info("人员库 {}-{} 中无数据或禁止进行修改删除，无需清理", haiouRepository.getId(), haiouRepository.getName());
                 } else {
                     cleanRepositoryList.add(haiouRepository);
                 }
